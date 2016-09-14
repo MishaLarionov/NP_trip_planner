16265bac344f54d2e24fc2ba6929415f5e4b6a23
Delaine Wendling <delaine.wendling@gmail.com>
2016-09-14 14:45:35 -0500

    Reloaded the cards on the trip view after deleting a card from the view

--- app/controllers/SingleTripCtrl.js
+++ app/controllers/SingleTripCtrl.js
@@ -2,10 +2,6 @@
 
 app.controller('SingleTripCtrl', function($scope, $routeParams, TripFactory, TrailFactory, $route){
 
-  //Need to define these things. Activities will have a trip Id on them that matches the routeParams
-  // $scope.activities
-  //Write a function in the TripFactory that gets the correct single trip from the database
-
     TripFactory.getSingleTrip($routeParams.tripId)
     .then((tripData)=>{
       console.log("trip data", tripData);
@@ -46,19 +42,6 @@ app.controller('SingleTripCtrl', function($scope, $routeParams, TripFactory, Tra
       $scope.cardAdded = true;
       console.log("activities?", $scope.activities);
     }
-
-    // // Generate initial model
-    // for (var i = 1; i <= 3; ++i) {
-    //     $scope.models.lists.A.push({label: "Item A" + i});
-    //     $scope.models.lists.B.push({label: "Item B" + i});
-    // }
-
-    // // Model to JSON for demo purpose
-    // $scope.$watch('models', function(model) {
-    //     $scope.modelAsJson = angular.toJson(model, true);
-    // }, true);
-
-
 });
 
 
